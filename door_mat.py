ae7941981d7ba46e4f177b6f774b5c8138a3d575
Philip Obiorah <philip.obiorah@outlook.com>
2023-09-04 23:46:24 +0100

    fixed bottom prin

--- door_mat.py
+++ door_mat.py
@@ -1,30 +1,24 @@
-import math
-
-
 def create_door_mat(rows, columns):
-    #Calculate the center position 
+    # Calculate the center position
     center = columns // 2
 
-    for i in range (rows):
+    for i in range(rows):
         if i < rows // 2:
-            #Top poart of the door mat
-            patten = ".|." *(2 * i + 1)
+            # Top part of the door mat
+            pattern = ".|." * (2 * i + 1)
         elif i == rows // 2:
-            #Center line with  'WELCOME'
-            patten = "WELCOME"
-
+            # Center line with 'WELCOME'
+            pattern = "WELCOME"
         else:
-            # Botom part of the door mat(mirror of the top part)
-            patten = ".|." * (2 * (rows - 1) - 1)
-
-        #Calculate the padding on each side
-        padding = (columns - len(patten)) // 2
-
-        #Print the line with padding
-        print('-' * padding + patten + '-' * padding)
-
+            # Bottom part of the door mat (mirror of the top part)
+            pattern = ".|." * (2 * (rows - i) - 1)
+        
+        # Calculate the padding on each side
+        padding = (columns - len(pattern)) // 2
+        
+        # Print the line with padding
+        print('-' * padding + pattern + '-' * padding)
 
 if __name__ == "__main__":
-    n, m = map(int, input("Enter size (N * M): ").split())
-    create_door_mat(n,m)
-    
+    n, m = map(int, input("Enter size (N x M): ").split())
+    create_door_mat(n, m)
